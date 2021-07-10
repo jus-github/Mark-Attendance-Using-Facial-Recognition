5dee90acd006b1901d512cafeac8f85d7bc827ba
jasleen <jasleen0711kochar@gmail.com>
2021-07-10 19:50:20 +0530

    Done..git add .

---
+++ basics.py
@@ -0,0 +1,34 @@
+# cmake  ,, opencv-python .. numpy,dlib=19.18.0 , facerecognition
+
+
+import cv2
+import  numpy as np
+
+# import face_recognition
+import cv2
+import face_recognition
+
+# imgElon = face_recognition.load_image_file('ImagesBasic/ElonMusk.jpg')
+# imgElon = cv2.cvtColor(imgElon, cv2.COLOR_BGR2RGB)
+imgJas = face_recognition.load_image_file('ImagesBasic/Jasleen.jpg.jpg')
+imgJas = cv2.cvtColor(imgJas, cv2.COLOR_BGR2RGB)
+imgTest = face_recognition.load_image_file('ImagesBasic/billgates.jpg')
+imgTest = cv2.cvtColor(imgTest, cv2.COLOR_BGR2RGB)
+
+faceLoc = face_recognition.face_locations(imgJas)[0]
+encodeElon = face_recognition.face_encodings(imgJas)[0]
+cv2.rectangle(imgJas, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (255, 0, 255), 2)
+
+faceLocTest = face_recognition.face_locations(imgTest)[0]
+encodeTest = face_recognition.face_encodings(imgTest)[0]
+cv2.rectangle(imgTest, (faceLocTest[3], faceLocTest[0]), (faceLocTest[1], faceLocTest[2]), (255, 0, 255), 2)
+
+results = face_recognition.compare_faces([encodeElon], encodeTest)
+faceDis = face_recognition.face_distance([encodeElon], encodeTest)
+print(results, faceDis)
+cv2.putText(imgTest, f'{results} {round(faceDis[0], 2)}', (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+#
+cv2.imshow('Jasleen', imgJas)
+cv2.imshow('Elon Test', imgTest)
+# cv2.imshow('Webcam' , imgElon)
+cv2.waitKey(0)
